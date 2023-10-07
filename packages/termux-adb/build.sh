16cfe6786f783ed77bb0931b86e6639f511277fd
nohajc <nohajc@gmail.com>
2023-10-07 20:44:11 +0200

    termux-adb v0.2.1 rev1 (android-tools 34.0.4)

--- packages/termux-adb/build.sh
+++ packages/termux-adb/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/nohajc/termux-adb
 TERMUX_PKG_DESCRIPTION="Run adb and fastboot in Termux without root permissions"
 TERMUX_PKG_LICENSE="Apache-2.0, MIT"
 TERMUX_PKG_MAINTAINER="nohajc"
-TERMUX_PKG_VERSION=0.2.0
-TERMUX_PKG_REVISION=6
-TERMUX_PKG_GIT_BRANCH="master"
-TERMUX_PKG_SRCURL=https://github.com/nohajc/vendor-adb-patched/archive/refs/heads/master.zip
+TERMUX_PKG_VERSION=0.2.1
+TERMUX_PKG_REVISION=1
+TERMUX_PKG_GIT_BRANCH="new"
+TERMUX_PKG_SRCURL=https://github.com/nohajc/vendor-adb-patched/archive/refs/heads/new.zip
 #TERMUX_PKG_SRCURL=file:///home/builder/termux-packages/termux-dev/android-tools.git
 TERMUX_PKG_SHA256=SKIP_CHECKSUM
 TERMUX_PKG_AUTO_UPDATE=true
@@ -16,6 +16,8 @@ termux_step_pre_configure() {
 	termux_setup_protobuf
 	termux_setup_golang
 	termux_setup_rust
+
+	LDFLAGS+=" $($TERMUX_SCRIPTDIR/packages/libprotobuf/interface_link_libraries.sh)"
 }
 
 termux_step_make() {
