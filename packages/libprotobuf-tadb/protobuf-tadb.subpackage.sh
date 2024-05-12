c035d9a040d0b8d09a76c2becca151fbdfe3828d
nohajc <nohajc@gmail.com>
2024-05-12 15:57:33 +0200

    create package libprotobuf-tadb-core with renamed library

---
+++ packages/libprotobuf-tadb/protobuf-tadb.subpackage.sh
@@ -0,0 +1,8 @@
+TERMUX_SUBPKG_INCLUDE="
+bin/
+lib/libprotobuf-tadb-lite.so*
+lib/libprotoc-tadb.so*
+lib/pkgconfig/protobuf-lite.pc
+"
+TERMUX_SUBPKG_DESCRIPTION="Compiler for protocol buffer definition files"
+
